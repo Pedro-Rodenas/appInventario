e569a703e18f9afa8b9ad2f6e9b8fa96211f53d4
Pedro Rodenas <pedrorodenas0506@gmail.com>
2025-11-04 09:18:46 -0500

    Corrijo startup.sh para usar PHP interno en Azure

--- startup.sh
+++ startup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Script de inicio para Azure App Service con Laravel (Nginx)
-# Configura Nginx para servir la carpeta public/
-
-# Elimina el directorio html predeterminado (si existe)
-rm -rf /home/site/wwwroot/html
-
-# Crea un enlace simbólico para que Nginx apunte a public/
-ln -s /home/site/wwwroot/public /home/site/wwwroot/html
-
-# Inicia Nginx en primer plano
-nginx -g "daemon off;"
+cd /home/site/wwwroot
+php -S 0.0.0.0:8000 -t public
